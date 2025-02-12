c6500b615d7cb6869771a85d81517eaed516d03b
Dan Knights <danknights95@gmail.com>
2025-02-12 22:04:59 +0000

    a

--- packages/js/core/loader.js
+++ packages/js/core/loader.js
@@ -1,18 +1,47 @@
 #!/usr/bin/env node
 import { spawnSync } from "node:child_process";
 import { getAbsoluteTargetExePath, getTargetTriple } from "./target.js";
+import { createRequire } from "node:module";
+import path from "node:path";
 
-const target = getTargetTriple();
-const targetExePath = getAbsoluteTargetExePath(target);
+const require = createRequire(import.meta.url);
 
-const result = spawnSync(targetExePath, process.argv.slice(2), {
+const archMap = {
+  x64: "x86_64",
+  arm64: "aarch64",
+};
+
+const platformMap = {
+  win32: "pc-windows-msvc",
+  darwin: "apple-darwin",
+  linux: "unknown-linux-gnu",
+};
+
+const archName = archMap[process.arch];
+const platformName = platformMap[process.platform];
+
+if (!archName) {
+  throw new Error(`[changenog]: unsupported architecture: ${process.arch}`);
+}
+
+if (!platformName) {
+  throw new Error(`[changenog]: unsupported platform: ${process.platform}`);
+}
+
+const target = `${archName}-${platformName}`;
+
+const absoluteTargetExePath = require.resolve(
+  path.join(`@gh-action-test/${target}`, "changenog")
+);
+
+const result = spawnSync(absoluteTargetExePath, process.argv.slice(2), {
   stdio: "inherit",
 });
 
 if (result.error) {
   console.error(
     `[changenog]: failed to execute binary for target: ${target}.  ensure the` +
-      `correct optional dependency is installed (@changenog/${target})\n`
+      ` correct optional dependency is installed (@changenog/${target})\n`
   );
 
   throw result.error;
